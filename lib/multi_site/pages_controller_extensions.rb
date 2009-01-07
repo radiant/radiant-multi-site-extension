ae9c26178a7f4931d5335df2a1cb02f6e369cb17
Sean Cribbs <seancribbs@gmail.com>
2009-01-07 16:59:00 -0500

    Refactor controller hooks, fix missing scenario thing.

--- lib/multi_site/pages_controller_extensions.rb
+++ lib/multi_site/pages_controller_extensions.rb
@@ -2,10 +2,12 @@ module MultiSite::PagesControllerExtensions
   def self.included(base)
     base.class_eval {
       alias_method_chain :index, :root
-      alias_method_chain :clear_model_cache, :site
       alias_method_chain :continue_url, :site
-      %w{remove}.each do |m|
-        alias_method_chain m.to_sym, :back
+      alias_method_chain :remove, :back
+      responses.destroy.default do 
+        return_url = session[:came_from]
+        session[:came_from] = nil
+        redirect_to return_url || admin_pages_url(:root => model.root.id)
       end
     }
   end
@@ -14,40 +16,21 @@ module MultiSite::PagesControllerExtensions
     if params[:root] # If a root page is specified
       @homepage = Page.find(params[:root])
       @site = @homepage.root.site
-    elsif @site = Site.find(:first, :order => "position ASC") # If there is a site defined
+    elsif @site = Site.first(:order => "position ASC") # If there is a site defined
       if @site.homepage
         @homepage = @site.homepage
-      else
-        index_without_root
       end
-    else # Just do the default
-      index_without_root
     end
+    @homepage ||= Page.find_by_parent_id(nil)
+    response_for :plural
   end
 
   def remove_with_back
-    @page = Page.find(params[:id])
-    if request.post?
-      announce_pages_removed(@page.children.count + 1)
-      @page.destroy
-      return_url = session[:came_from]
-      session[:came_from] = nil
-      if return_url && return_url != page_index_url(:root => @page)
-        redirect_to return_url
-      else
-        redirect_to page_index_url(:page => @page.parent)
-      end
-    else
-      session[:came_from] = request.env["HTTP_REFERER"]
-    end
+    session[:came_from] = request.env["HTTP_REFERER"]
+    super
   end
-
-  def clear_model_cache_with_site
-    Page.current_site ||= @site || @page.root.site
-    clear_model_cache_without_site
-  end
-
+  
   def continue_url_with_site(options={})
-    options[:redirect_to] || (params[:continue] ? page_edit_url(:id => model.id) : page_index_url(:root => model.root.id))
+    options[:redirect_to] || (params[:continue] ? edit_admin_page_url(model) : admin_pages_url(:root => model.root.id))
   end
 end
--- spec/models/multi_site_response_cache_spec.rb
+++ spec/models/multi_site_response_cache_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ResponseCache do
-  scenario :sites
+  dataset :sites
   
   before do
     @cache = ResponseCache.new
