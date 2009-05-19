240bdbe7dfa235e673bb3cf2291a05b792807f20
Kunal Shah <kunal@digitalpulp.com>
2009-05-19 15:28:31 -0400

    Radiant Core appears to be doing the same thing

--- lib/multi_site/site_controller_extensions.rb
+++ lib/multi_site/site_controller_extensions.rb
@@ -2,7 +2,6 @@ module MultiSite::SiteControllerExtensions
   def self.included(base)
     base.class_eval do
       before_filter :set_site
-      alias_method_chain :dev?, :domain
     end
   end
   
@@ -10,10 +9,4 @@ module MultiSite::SiteControllerExtensions
     Page.current_site = Site.find_for_host(request.host)
     true
   end
-  
-  private
-    def dev_with_domain?
-      prefix = @config["dev.host"] || "dev"
-      request.host =~ %r{^#{prefix}\.}
-    end
 end
