2dd221861f4436991792c33b010c6a4944956253
Kunal Shah <kunal@digitalpulp.com>
2009-05-20 10:49:18 -0400

    Update routing for admin namespace

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,10 +1,10 @@
 module SitesHelper
   def order_links(site)
     returning String.new do |output|
-      output << link_to(image("move_to_top.png", :alt => "Move to top"), move_to_top_site_path(site), :method => :put)
-      output << link_to(image("move_higher.png", :alt => "Move up"), move_higher_site_path(site), :method => :post)
-      output << link_to(image("move_lower.png", :alt => "Move down"), move_lower_site_path(site), :method => :post)
-      output << link_to(image("move_to_bottom.png", :alt => "Move to bottom"), move_to_bottom_site_path(site), :method => :put)
+      output << link_to(image("move_to_top.png", :alt => "Move to top"), move_to_top_admin_site_path(site), :method => :put)
+      output << link_to(image("move_higher.png", :alt => "Move up"), move_higher_admin_site_path(site), :method => :post)
+      output << link_to(image("move_lower.png", :alt => "Move down"), move_lower_admin_site_path(site), :method => :post)
+      output << link_to(image("move_to_bottom.png", :alt => "Move to bottom"), move_to_bottom_admin_site_path(site), :method => :put)
     end
   end
 end
