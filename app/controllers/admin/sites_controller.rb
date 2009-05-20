9269f33a4fea0e33143adf0bb340e4d5846884c5
Kunal Shah <kunal@digitalpulp.com>
2009-05-20 10:48:42 -0400

    Use your helper

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,4 +1,6 @@
 class Admin::SitesController < Admin::ResourceController
+  helper :sites
+  
   %w(move_higher move_lower move_to_top move_to_bottom).each do |action|
     define_method action do
       model.send(action)
