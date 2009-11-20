b221cf57e9fa6ed072c7e0e6081f0f0a83384eca
Josh French <josh@digitalpulp.com>
2009-11-20 17:10:51 -0500

    Add regionsets

--- multi_site_extension.rb
+++ multi_site_extension.rb
@@ -18,7 +18,6 @@ class MultiSiteExtension < Radiant::Extension
     end
   end
 
-
   def activate
     require 'multi_site/route_extensions'
     require 'multi_site/route_set_extensions'
@@ -27,9 +26,26 @@ class MultiSiteExtension < Radiant::Extension
     Admin::PagesController.send :include, MultiSite::PagesControllerExtensions
     admin.pages.index.add :bottom, "site_subnav"
     admin.nav[:settings] << admin.nav_item("Sites", "Sites", "/admin/sites")
+    @sites = load_default_regions
   end
 
   def deactivate
+  end
+
+  def load_default_regions
+    Radiant::AdminUI.class_eval { attr_accessor :sites }
+    admin.sites = returning OpenStruct.new do |sites|
+      sites.index = Radiant::AdminUI::RegionSet.new do |index|
+        index.header.concat %w{name_th match_th base_th modify_th order_th}
+        index.row.concat %w{name_td match_td base_td modify_td order_td}
+      end
+      sites.edit = Radiant::AdminUI::RegionSet.new do |edit|
+        edit.main.concat %w{edit_header edit_form}
+        edit.form.concat %w{edit_name edit_match edit_base edit_homepage}
+        edit.form_bottom.concat %w{edit_buttons}
+      end
+      sites.new = sites.edit
+    end
   end
 
 end
