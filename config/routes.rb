9b5da2d97852f0f17a27583942e80fb3150fb8ec
Wes Gamble <weyus@att.net>
2011-06-03 14:50:44 -0500

    Handle Rails 2.3.11 and Radiant deprecation warnings (Kernel#returning -> Object#tap, define_routes -> config/routes.rb)

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+define_routes do |map|
+  map.namespace :admin, :member => { :remove => :get } do |admin|
+    admin.resources :sites, :member => {
+      :move_higher => :post,
+      :move_lower => :post,
+      :move_to_top => :put,
+      :move_to_bottom => :put
+    }
+  end
+end
