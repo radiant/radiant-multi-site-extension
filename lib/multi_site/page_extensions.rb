713b4e4fd9352267ec1ecf21fc219023a542569c
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2012-01-30 10:45:48 -0800

    minimal adjustments to make multi_site work with Radiant 1.0 and find_by_path

--- lib/multi_site/page_extensions.rb
+++ lib/multi_site/page_extensions.rb
@@ -7,18 +7,18 @@ module MultiSite::PageExtensions
     }
     base.extend ClassMethods
     class << base
-      alias_method_chain :find_by_url, :sites
+      alias_method_chain :find_by_path, :sites
     end
   end
   
   module ClassMethods
-    def find_by_url_with_sites(url, live=true)
+    def find_by_path_with_sites(url, live=true)
       root = find_by_parent_id(nil)
       if self.current_site.is_a?(Site)
         root = self.current_site.homepage
       end
       raise Page::MissingRootPageError unless root
-      root.find_by_url(url, live)
+      root.find_by_path(url, live)
     end
   end
   
